46b7635e0d6677b6039774ce217bf3b27a78fe37
Daniel Cazé <danielctlima@gmail.com>
2022-11-02 16:13:08 -0300

    radix dialog portal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,26 @@
 import * as S from "./styles";
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
     <S.HeaderContainer>
       <S.HeaderContent>
         <img src={logoImg} alt="" />
-        <S.NewTransactionButton>New Transaction</S.NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>New Transaction</S.NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>New Transaction</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
   )
